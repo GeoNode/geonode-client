644397b5feeb52c06735843f15cba182b9a513eb
milafrerichs <mila.frerichs@gmail.com>
2016-10-17 11:59:18 +0200

    Add test for rendering
    it should be inside a div and the component is rendered
    should habve a layerlist with one basemap layer included

--- tests/geonodeviewer.test.js
+++ tests/geonodeviewer.test.js
@@ -18,4 +18,24 @@ describe('GeoNodeViewer', () => {
 		const geonodeviewer = rendererWithIntl( <GeoNodeViewer config={config}/> );
 		assert.isDefined(ReactTestUtils.isCompositeComponent(geonodeviewer));
 	});
+	it('has a root div', () => {
+		const renderer = ReactTestUtils.createRenderer();
+		renderer.render(<GeoNodeViewer config={config} />);
+		let result = renderer.getRenderOutput();
+		assert.equal(result.type, 'div')
+	});
+  describe('one layer', () => {
+    beforeEach(() => {
+      config = { map: {
+        layers: [{"opacity": 1.0, "group": "background", "name": "mapnik", "visibility": true, "source": "0", "fixed": true, "type": "OpenLayers.Layer.OSM"}]
+      },
+      sources: { "0": {"ptype": "gxp_osmsource"} }
+      };
+    });
+    it('the layer list includes one layer', () => {
+			const geonodeviewer = ReactTestUtils.renderIntoDocument(<IntlProvider locale="en"><GeoNodeViewer config={config}/></IntlProvider>);
+      var contents = ReactTestUtils.scryRenderedDOMComponentsWithClass(geonodeviewer, 'layer-list');
+			assert.equal(contents[0].textContent,'Base Mapsmapnik');
+    });
+	});
 });
