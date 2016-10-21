31a1121d501b1727a635c95fdb1788b392821a38
milafrerichs <mila.frerichs@gmail.com>
2016-10-21 18:38:57 +0200

    Read the url from the queryString
    if a config is detected update the map
    if a new config is input in the field reload the page with the new query

--- GeonodeDebug.jsx
+++ GeonodeDebug.jsx
@@ -10,7 +10,26 @@ class GeoNodeViewerDebug extends React.Component {
     super(props);
     this.state = { config: this.props.config };
     this.config = JSON.stringify(this.props.config);
-    this.configUrl = '';
+    const qry = this.uriVars();
+    if(qry.config) {
+      this.fetchConfigFromUrl(this.proxyUrl(qry.config));
+      this.configUrl = qry.config;
+    } else {
+      this.configUrl = '';
+    }
+  }
+  proxyUrl(url) {
+    return ('https://cors-anywhere.herokuapp.com/'+url);
+  }
+  uriVars(str) {
+    let vars = {};
+    str = str || window.location.href;
+    var hashes = str.slice(str.indexOf('?') + 1).split('&');
+    for(var i=0,len=hashes.length;i<len;i++){
+      var hash = hashes[i].split('=');
+      vars[hash[0]] = decodeURIComponent(hash[1]);
+    }
+    return vars;
   }
   fetchConfigFromUrl(url) {
     fetch(url).then((response) => {
@@ -25,7 +44,8 @@ class GeoNodeViewerDebug extends React.Component {
   }
   configUrlChange(config) {
     this.configUrl = config;
-    this.fetchConfigFromUrl('https://cors-anywhere.herokuapp.com/'+this.configUrl);
+    this.fetchConfigFromUrl(this.proxyUrl(config));
+    window.location.search = '?config='+config;
   }
   render() {
     return (
