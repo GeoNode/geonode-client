4e02a98e0c7dcbeca2cd7f085782c27ba60a1322
Bart van den Eijnden <bartvde@boundlessgeo.com>
2016-11-03 17:28:05 +0100

    Change zoom to 3

--- geonode.jsx
+++ geonode.jsx
@@ -68,7 +68,7 @@ var map = new ol.Map({
   layers: [
     new ol.layer.Tile({title: 'OpenStreetMap', source: new ol.source.OSM()})
   ],
-  view: new ol.View({center: [0, 0], zoom: 1})
+  view: new ol.View({center: [0, 0], zoom: 3})
 });
 
 class GeoNodeViewer extends React.Component {
