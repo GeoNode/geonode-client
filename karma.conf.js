dbc6328e9db48e1b5b949f3c9a9b853b15c629b3
milafrerichs <mila.frerichs@gmail.com>
2016-11-25 10:17:43 +0100

    Change test framework to karma
    use karma and webpack to generate the tests
    this takes longer since it has to transpile all files first but works
    coverage does not work yet

---
+++ karma.conf.js
@@ -0,0 +1,30 @@
+var webpackConfig = require('./webpack.config');
+webpackConfig.devtool = 'inline-source-map';
+
+module.exports = function (config) {
+  config.set({
+    browsers: [ 'PhantomJS' ],
+    files: [
+      'tests/tests.bundle.js',
+      { pattern: 'src/**/*', watched: false, included: false, served: true, nocache: true },
+      { pattern: 'dist/**/*.js', watched: false, included: false, served: true, nocache: true }
+    ],
+    preprocessors: {
+      'tests/tests.bundle.js': [ 'webpack', 'sourcemap' ]
+    },
+    frameworks: ['intl-shim', 'mocha'],
+    reporters: [ 'mocha', 'coverage' ],
+    webpack: webpackConfig,
+    webpackServer: {
+      noInfo: true
+    },
+    autoWatch: false,
+		coverageReporter: {
+      dir: 'reports',
+      reporters:[
+        {type: 'html', subdir: 'html/'},
+        {type: 'lcovonly', subdir: 'coverage/', file: 'lcov.info'},
+      ]
+    }
+  });
+};
