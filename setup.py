ef14314302e80a58ef5e1f1e080e302eee4c8536
Mila Frerichs <mila.frerichs@gmail.com>
2016-12-13 17:00:01 +0100

    Update and fix django pip package creation (#71)
    * Remove dist folder from github
    
    * Update package generation
    
    use npm to create all the files and the folder structure
    copy all files that are necessary
    create a makefile to simple release new versions
    use a VERSION file to keep the version number in one file (package.json
    does not use it yet)
    
    * Update boundless sdk to fix test
    
    adding the same layer twice resulting in an error
    but this should be possible, so the SDK got fixed

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'VERSION')) as version_file:
+        version = version_file.read().strip()
 
 setup(
     name='django-geonode-client',
-    version='0.0.1',
+    version=version,
     author='Mila Frerichs',
     author_email='mila.frerichs@gmail.com',
     url='https://github.com/GeoNode/geonode-client',
