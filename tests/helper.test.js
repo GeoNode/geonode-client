22f30ffd8addec306d6127331653820689518227
Mila Frerichs <mila.frerichs@gmail.com>
2016-12-23 16:19:50 +0100

    Fix the possibility of a trailing slash in the server url (#78)
    Add methods to remove trailing slash if a trailing is detected
    add tests for both the methods and the saveMap method

--- tests/helper.test.js
+++ tests/helper.test.js
@@ -3,14 +3,31 @@ import raf from 'raf';
 raf.polyfill();
 import {assert} from 'chai';
 
-import {getCRSFToken} from '../src/helper';
+import {getCRSFToken, hasTrailingSlash, removeTrailingSlash} from '../src/helper';
 
 describe('getCRSFToken', () => {
-  let map;
   beforeEach(function() {
-		document.cookie = "csrftoken=1;"
+		document.cookie = "csrftoken=1;";
   });
 	it('returns csrftoken from cookie', () => {
 		assert.equal(getCRSFToken(),'1');
+	});
+});
+
+describe('hasTrailingSlash', () => {
+	it('returns true if a trailing slash is detected', () => {
+		assert.equal(hasTrailingSlash('test/'),true);
+	});
+	it('returns false if no trailing slash is detected', () => {
+		assert.equal(hasTrailingSlash('test'),false);
+	});
+});
+
+describe('removeTrailingSLash', () => {
+	it('returns str without trailing slash', () => {
+		assert.equal(removeTrailingSlash('test/'),'test');
+	});
+	it('returns str without trailing slash', () => {
+		assert.equal(removeTrailingSlash('test'),'test');
 	});
 });
