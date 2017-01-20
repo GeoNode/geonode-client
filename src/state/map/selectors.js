4a6fec164f35a2bc4bbb7f189a106e69bbf1072f
milafrerichs <mila.frerichs@gmail.com>
2017-01-20 10:44:33 +0100

    Add new selectors for map
    selectors separates the way of getting the state for specific items
    jus tprive methods instead of fiddling with the internal way the state
    is stored

---
+++ src/state/map/selectors.js
@@ -0,0 +1,19 @@
+
+function isError(state) {
+  return (state.map.save.error && !state.map.save.success) ? true : false;
+}
+export function isSaving(state) {
+  return state.map.save.success ? false : true;
+}
+export function success(state) {
+  return state.map.save.success;
+}
+export function error(state) {
+  return isError(state);
+}
+export function errorMessage(state) {
+  return isError(state) ? state.map.save.errorMessage : undefined;
+}
+export function getMapId(state) {
+  return state.map.id;
+}
